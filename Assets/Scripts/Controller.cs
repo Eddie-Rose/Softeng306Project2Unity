7f977ef9f011f4dc98399b4ede7513f83dadab2d
Kerwin Sun <ksun182@UoA.auckland.ac.nz>
2018-09-28 14:25:22 +1200

    implemented basic timer event

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour {
+    public GameObject dot;
+    public float timedEventA = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        Timer();
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Timer();
 	}
+
+    void DoDetect()
+    {
+
+        Debug.Log("Hi there");
+        dot = GameObject.Find("BLUE DOT");
+        SimpleController other = (SimpleController)dot.GetComponent(typeof(SimpleController));
+        other.ChangeColor();
+        
+    }
+
+    void Timer() {
+
+        timedEventA -= Time.deltaTime;
+
+        if (timedEventA <= 0.0f)
+        {
+            Debug.Log("Bye There");
+            DoDetect();
+            timedEventA = 5.0f;
+        }
+
+    }
 }
---
+++ Assets/Scripts/SimpleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleController : MonoBehaviour {
+
+    SpriteRenderer m_SpriteRenderer;
+    Color m_NewColor;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    public void ChangeColor() {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+
+        m_SpriteRenderer.color = Color.green;
+        Debug.Log("Color Changed, Event Detected");
+
+    }
+}
