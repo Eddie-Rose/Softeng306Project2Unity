9fe44abff58b56a7b568792f2d29b869c0c31a62
Your Name <nikhilreddy24@hotmail.com>
2018-09-30 00:19:01 +1200

    Added Npc including random movement, Additionally added tile palatees and floor sorting layers

---
+++ Assets/Scripts/NPC behaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCbehaviour : MonoBehaviour{
+
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidBody;
+
+    public bool isWalking;
+
+    public float walkTime;
+    private float walkCounter;
+    public float waitTime;
+    private float waitCounter;
+
+    // Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        waitCounter = waitTime;
+        walkCounter = walkTime;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
