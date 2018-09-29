8863f59a3e83a6870de88f2bd1da020292e81485
Your Name <nikhilreddy24@hotmail.com>
2018-09-29 17:41:39 +1200

    Added Idea Generation file

---
+++ Assets/Scripts/IdeaGeneration.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdeaGeneration : MonoBehaviour {
+    public string IdeaTitle { get; set; }
+    public int CostOfIdea { get; set; }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
