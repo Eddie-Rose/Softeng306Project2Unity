0a0e17bda4c50e7e08e95f9616866c311c717dd2
Edwin Roesli <eddie.roesli@gmail.com>
2018-10-01 09:11:08 +1300

    Prototype UI for proposalList

---
+++ Assets/Scripts/ScrollViewAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewAdapter : MonoBehaviour {
+
+    public RectTransform prefab;
+    public Text countText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void UpdateItems(){
+
+    } 
+	
+	void FetchItemModels (int count, Action onDone) {
+		yeild return new WaitForSeconds(2f);
+	}
+
+   
+}
