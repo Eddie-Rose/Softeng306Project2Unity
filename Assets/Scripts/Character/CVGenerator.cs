ffd628de1986d4ae00688a09dddb76dad807b5dc
Rodger Gu <RodgerG@hotmail.co.nz>
2018-10-01 21:17:57 +1300

    Added CV and NPC generation classes

---
+++ Assets/Scripts/Character/CVGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class CVGenerator : MonoBehaviour
+{
+    private string name;
+    private Gender gender;
+    private byte age;
+    private Nationality nationality;
+    private DateTime time;
+    private string summary;
+    private float skill;
+    private float teamwork;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rollStats();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void rollStats()
+    {
+        double total = 10 + new System.Random().NextDouble() * 5;
+    }
+}
+
+enum Gender
+{
+    MALE, FEMALE, UNDEFINED
+}
+
+enum Nationality
+{
+    NEW_ZEALANDER, CANADIAN, AMERICAN, FRENCH
+}
