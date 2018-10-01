b872051d915dad10e1b8fe1eb4f0b4c07f03a657
Rodger Gu <RodgerG@hotmail.co.nz>
2018-10-01 20:19:39 +1300

    Added script for depth checking

---
+++ Assets/Scripts/DepthTriggerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthTriggerScript : MonoBehaviour {
+    private SpriteRenderer spR;
+
+	// Use this for initialization
+	void Start () {
+        spR = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SpriteRenderer colSPR = other.GetComponent<SpriteRenderer>();
+        spR.sortingOrder = colSPR.sortingOrder;
+        colSPR.sortingOrder = colSPR.sortingOrder + 1;
+
+        Debug.Log("Collision Event Triggered");
+    }
+
+}
