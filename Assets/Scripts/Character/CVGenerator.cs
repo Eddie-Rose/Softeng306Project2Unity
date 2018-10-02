7f6191dc98f964cb420a6df0eba2e884d4a7dbb7
LucasSherlock <lshe485@aucklanduni.ac.nz>
2018-10-02 14:06:56 +1300

    added name and summary assignment

--- Assets/Scripts/Character/CVGenerator.cs
+++ Assets/Scripts/Character/CVGenerator.cs
@@ -12,6 +12,27 @@ public class CVGenerator : MonoBehaviour
     private float skill;
     private float teamwork;
 
+    private static string[] names = 
+    {
+        "John Clark",
+        "Jennifer Adams",
+        "Vanessa Wu",
+        "Tane Phillips",
+        "Henri DuPont",
+        "Abigail Bantham"
+    };
+
+    private static string[] summaries = 
+    {
+        "I am a very social person who enjoys working as part of a diverse and interesting team. I have strong communication and leadership skills. I have a degree in Marketing.",
+        "I am a focused person who excels at producing high quality work. My area of expertise is in marketing and I am very good at presenting an idea in a way which is pleasing to the target audience.",
+        "I am a calm and fair person. I have experience working in HR and especially in conflict mediation. I believe that a harmonious workforce is a productive workforce.",
+        "I am a hard-working and driven person who will always give 110% to any team and/or project that I am a part of. I work best as part of a small and focused team.",
+        "I am an independent worker who loves to overcome challenges in my work. I am great at taking initiative and am happiest when I am given the freedom to work my way.",
+        "I am a laid-back and relaxed member of the workforce who finds great pleasure in working as part of a large and varied team. I love to get involved in social activites in addition to my work responsibilities."
+    };
+
+
 
     // Use this for initialization
     void Start()
@@ -55,6 +76,18 @@ public class CVGenerator : MonoBehaviour
 
         //roll the nationality.
         nationality = (Nationality) new System.Random().Next(0, 3);
+
+        //assign a name.
+        name = names[new System.Random().Next(names.Length)];
+
+        Console.WriteLine(name);
+
+
+        //assign a summary.
+        summary = summaries[new System.Random().Next(summaries.Length)];
+
+
+        Console.WriteLine(summary);
     }
 }
 
