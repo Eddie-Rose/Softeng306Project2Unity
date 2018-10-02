e620e90552f7e55fbaee6393ed4d3a813b68f79d
cornierspring <cornierspring@gmail.com>
2018-10-02 15:09:50 +1300

    cvadapter

--- Assets/Scripts/Character/CVAdapter.cs
+++ Assets/Scripts/Character/CVAdapter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CVGenerator : MonoBehaviour {
+public class CVAdapter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
