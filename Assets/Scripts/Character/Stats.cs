abba7aaa056779439dfd60336cad9e25a45c19dd
cornierspring <cornierspring@gmail.com>
2018-10-15 12:10:21 +1300

    start

---
+++ Assets/Scripts/Character/Stats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+    public int seed;
+    public int happiness;
+    public int skill;
+    public int teamwork;
+    public string gender;
+    public string ethnicity;
+    public string position;
+
+    void Update() {}
+
+    void Start() {
+
+    }
+
+    public int compatiblility(Stats otherNPC)
+    {
+        return 0;
+    }
+
+    public int generateSeed()
+    {
+        return 0;
+    }
+
+}
+
