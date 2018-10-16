7059d3364bb976bdd7913025c2247f9d34a59eb0
LucasSherlock <lshe485@aucklanduni.ac.nz>
2018-10-16 16:32:00 +1300

    added to stats script and a digraph implementation for employee relationships

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -4,6 +4,53 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+    public int seed;
+    public int happiness;
+    public string name;
+    public string ethnicity;
+    public string gender;
+    public string age;
+    public string position;
+    public int teamwork;
+    public int skill;
+    //public List<Stats> friends = new List<Stats>();
+    public List<InteractionGraph.Relationship> relationships = new List<InteractionGraph.Relationship>();
+
+    //void Update() {}
+
+    void Start()
+    {
+
+    }
+
+
+
+    public int compatiblility(Stats otherNPC) {
+
+        int comp = 0;
+        if (ethnicity.Equals(otherNPC.ethnicity)) {
+
+            comp += 5;
+            //comp += Random.Range(-2, 8);
+
+        } else {
+
+            //comp += Random.Range(-4, 6);
+
+        }
+
+        if (gender.Equals(otherNPC.gender)) {
+            comp += 3;
+            //comp += Random.Range(-1, 5);
+        } else {
+            //comp += Random.Range(-2, 4);
+        }
+
+        comp += ((teamwork + otherNPC.teamwork) / 2);
+
+        return comp;
+    }
+
 
     public void killNPC()
     {
